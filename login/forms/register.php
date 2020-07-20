82774dd6b62c9ac6e4d1b5f66cdf310777c9f1bb
Eloy <58450310+EloyRDev@users.noreply.github.com>
2020-07-20 15:43:32 +0200

    Change between login and register by URL

---
+++ login/forms/register.php
@@ -0,0 +1,18 @@
+<fieldset>
+    <label>Username</label>
+    <input id="username" type="text" autocomplete="off" placeholder="CoolName80">
+</fieldset>
+<fieldset>
+    <label>Email</label>
+    <input id="email" type="text" autocomplete="off" placeholder="myemail@gmail.com">
+</fieldset>
+<fieldset>
+    <label>Password</label>
+    <input id="password" type="password">
+</fieldset>
+<fieldset>
+    <label>Confirm password</label>
+    <input id="Cpassword" type="password">
+</fieldset>
+<input id="register_f_SEND" type="button" value="Register">
+<p>You already have an account? <a href="index.php">Log in!</a></p>	
