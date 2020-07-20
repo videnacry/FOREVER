9b8780a814cfff7ecbd193318854dbfab86d1319
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-20 15:56:11 +0200

    Linked all NPM packages + JS main file

---
+++ general_wall/general_wall.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Develop Your Project with PHP</title>
+  <!--NPM Packages -->
+  <script src="../node_modules/jquery/dist/jquery.js"></script>
+  <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+  <link href="../node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
+</head>
+<body>
+  <!-- Own JS file -->
+  <script src="../src/js/app.js"></script>
+</body>
+</html>
