d5ef41682d97e5a90b23a566ffd1581669d31310
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-20 16:56:35 +0200

    First draft post container

--- general_wall/general_wall.php
+++ general_wall/general_wall.php
@@ -14,6 +14,28 @@
   <link href="../node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet"/>
 </head>
 <body>
+  <header>
+    <!-- Navbar by Beron -->
+  </header>
+  <main>
+    <!-- General wall -->
+    <div class="container my-3">
+      <div class="row mx-1">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8 border border-secondary p-3">
+          <div class="row mx-0">
+            <textarea name="" id="" cols="100" rows="4" class="border border-secondary p-3 rounded" placeholder="What are your thoughts..."></textarea>
+          </div>
+          <div class="row mx-0 mt-2">
+            <button type="button" class="btn btn-primary"></button>
+            <button type="button" class="btn btn-primary"></button>
+            <button type="submit" class="btn btn-primary">Post</button>
+          </div>
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+    </div>
+  </main>
   <!-- Own JS file -->
   <script src="../src/js/app.js"></script>
 </body>
