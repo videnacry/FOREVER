cde514e1ea64741ff01eeb587fb8c748be403a2a
Eloy <58450310+EloyRDev@users.noreply.github.com>
2020-07-20 13:43:34 +0200

    Login files

---
+++ login/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoTatoe</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="login.js" defer></script>
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+</head>
+<body>
+    <form>
+        <fieldset>
+            <label>Username</label>
+            <input type="text" autocomplete="off" placeholder="CoolName80">
+        </fieldset>
+        <fieldset>
+            <label>Email</label>
+            <input type="text" autocomplete="off" placeholder="myemail@gmail.com">
+        </fieldset>
+        <fieldset>
+            <label>Password</label>
+            <input type="pass">
+        </fieldset>
+    </form>
+</body>
+</html>
