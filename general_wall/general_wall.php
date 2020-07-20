137d74635d4dfb95de7d61e103042b01916b97d4
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-20 18:19:28 +0200

    Draft card existing post

--- general_wall/general_wall.php
+++ general_wall/general_wall.php
@@ -8,6 +8,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Develop Your Project with PHP</title>
+  <!-- Own stylesheet -->
+  <link href="../src/css/style.css" rel="stylesheet"/>
   <!-- Font awesome -->
   <script src="https://kit.fontawesome.com/de217cab6a.js" crossorigin="anonymous"></script>
   <!--NPM Packages -->
@@ -22,9 +24,10 @@
   <main>
     <!-- General wall -->
     <div class="container my-3">
+      <!-- Input for new post -->
       <div class="row mx-auto my-3">
         <div class="col-sm-2"></div>
-        <div class="col-sm-8 border border-secondary p-3 shadow-lg">
+        <div class="col-sm-8 border border-secondary p-3 shadow">
           <div class="row mx-0">
             <textarea name="" id="" cols="100" rows="3" class="border border-secondary p-3 rounded" placeholder="What are your thoughts?"></textarea>
           </div>
@@ -37,6 +40,18 @@
             </div>
             <div class="buttons-block-2">
               <button type="submit" class="btn btn-primary">Post</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+      <!-- Existing posts -->
+      <div class="row mx-auto mt-3">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8 border border-secondary p-3 shadow">
+          <img src="https://cdn.pixabay.com/photo/2014/09/11/18/23/london-441853_1280.jpg" alt="" class="img-fluid border border-secondary">
+          <div class="row mx-auto mt-3 border border-secondary">
+            <div class="img-cont border rounded-circle">
             </div>
           </div>
         </div>
