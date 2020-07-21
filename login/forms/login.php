b4cda1f1ed395f5bdbe52fcec0cc89fedd859f6f
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-21 15:28:02 +0200

    Completed design of register.php with Bootstrap

--- login/forms/login.php
+++ login/forms/login.php
@@ -18,10 +18,10 @@
 <body>
     <main>
         <div class="container">
-            <div class="row align-items-center login-cont">
+            <div class="row align-items-center form-cont">
                 <div class="col-md-3"></div>
                 <div class="col-md-6">
-                    <h3 class="text-center m-5">Login</h3>
+                    <h3 class="text-center m-5 text-primary">Login</h3>
                     <form action="">
                         <div class="form-group">
                             <fieldset>
