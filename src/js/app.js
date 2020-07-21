a2bb2905eafda144040b843f61dc072dc279ef21
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-21 08:15:58 +0200

    Issue with placeholder in contenteditable

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,27 @@
+const ele = document.getElementById('comment-box');
+
+// Get the placeholder attribute
+const placeholder = ele.getAttribute('data-placeholder');
+
+// Set the placeholder as initial content if it's empty
+(ele.innerHTML === '') && (ele.innerHTML = placeholder);
+ele.innerHTML = placeholder;
+
+ele.addEventListener('focus', function (e) {
+  const value = e.target.innerHTML;
+  value === placeholder && (e.target.innerHTML = '');
+});
+
+ele.addEventListener('blur', function (e) {
+  const value = e.target.innerHTML;
+  value === '' && (e.target.innerHTML = placeholder);
+});
+
+
 // Event listener to remove "fake" placeholder in content editable div
-$('#post-box').on('focus', function (e) {
-  $(e.target).text('');
-})
+// $('#post-box').on('focus', function (e) {
+//   $(e.target).text('');
+// })
 
 // Event listener to add "fake" placeholder in content editable div
 // $('#post-box').on('blur', function (e) {
