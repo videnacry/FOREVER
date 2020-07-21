61022f8d2c7136fc9217350644cbca0a0d9c72fb
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-07-21 20:14:10 +0200

    Removed lines for editable div (finally not used)

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-
 if (document.getElementById("profile")) {
 
     //-----------------------------------Modal to add a post----------------------------------//
@@ -14,7 +13,7 @@ if (document.getElementById("profile")) {
         transform: "translateY(-50%) translateX(-50%)",
         zIndex: "2"
     })
-    $("#modal-post-box").css("background-color","whitesmoke")
+    $("#modal-post-box").css("background-color", "whitesmoke")
     $(newPost).click(function () {
         $(closeModals).toggle()
         $(modalPost).toggle()
@@ -70,58 +69,14 @@ $("#formUpdateUser").submit(function (e) {
         method: "POST",
         url: "control-data/update-validation.php",
         data: data,
-        success: function (data) {
-        }
+        success: function (data) {}
     })
 })
-// Get input boxes
-// const ele1 = document.getElementById('post-box');
-// const ele2 = document.getElementById('comment-box');
-
-// Get the placeholder attributes
-// const placeholder1 = ele1.getAttribute('data-placeholder');
-// const placeholder2 = ele2.getAttribute('data-placeholder');
-
-// Set the placeholder as initial content if it's empty
-// ele1.innerHTML = placeholder1;
-// ele2.innerHTML = placeholder2;
-
-// Add event listeners
-// Focus
-// ele1.addEventListener('focus', function (e) {
-//   const value = e.target.innerHTML;
-//   value === placeholder1 && (e.target.innerHTML = '');
-// });
-// ele2.addEventListener('focus', function (e) {
-//   const value = e.target.innerHTML;
-//   value === placeholder2 && (e.target.innerHTML = '');
-// });
-// Blur
-// ele1.addEventListener('blur', function (e) {
-//   const value = e.target.innerHTML;
-//   value === '' && (e.target.innerHTML = placeholder1);
-// });
-// ele2.addEventListener('blur', function (e) {
-//   const value = e.target.innerHTML;
-//   value === '' && (e.target.innerHTML = placeholder2);
-// });
-
-// Replace inserted "div" in content-editable with "p"
-// document.execCommand('defaultParagraphSeparator', false, 'p');
-
-// Test
-// $('#post').on('click', function (e) {
-//   e.preventDefault();
-//   console.log($('#post-box').html());
-//   $('#post-box').text($('#post-box').data('placeholder'));
-// $('#test').html(($(this).parent().parent().prev().children().eq(0).html()));
-// })
-
 
 //----------------------------- General wall ----------------------------------//
 
 // Load posts
-function loadPosts(index=-1) {
+function loadPosts(index = -1) {
 
 }
 
@@ -145,16 +100,18 @@ $("#gif-input").on("input", e => {
 function loadGifs(search = "") {
     const box = $('.gif-box');
     $(".gif-element").remove();
-    
+
     box.append(`<img class="gif-element" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.onemansblog.com%2Fwp-content%2Fuploads%2F2016%2F05%2FSpinner-Loading.gif&f=1&nofb=1" alt="Loading..." Width="170px" style="border:none">`);
-    $.post("../general_wall/getGifs.php", { search: search }, data => {
+    $.post("../general_wall/getGifs.php", {
+        search: search
+    }, data => {
         const gifs = JSON.parse(data).data;
         $(".gif-element").remove();
         console.log(gifs)
-        for(const gif of gifs) 
+        for (const gif of gifs)
             box.append(`<img class="gif-element" src="${gif.images.preview_gif.url}" data-src="${gif.images.downsized.url}" alt="${gif.title}" Width="170px">`);
-    
-        $(".gif-element").click(e => { 
+
+        $(".gif-element").click(e => {
             $('#img-new-post').attr('src', "")
             $('#img-new-post').attr('src', e.target.dataset.src)
             $('#img-new-post').removeClass("d-none");
