0d6b8c2679c42bf94367e438fa0f3c51b91252b9
Ezequiel <ezemgaray@gmail.com>
2020-07-22 16:21:57 +0200

    update general_wall/actions/click-like.php

--- general_wall/actions/click-like.php
+++ general_wall/actions/click-like.php
@@ -3,23 +3,28 @@ session_start();
 require("../../src/php/functions.php");
 
 if($_POST){
-   // Update posts.json
    $arrayPosts = json_decode(file_get_contents("../../JSON/posts.json"));
-   $index = findIndex($arrayPosts, "id", $_POST["post-id"]);
-   if($arrayPosts[$index]){
+   $indexPosts = findIndex($arrayPosts, "id", $_POST["post-id"]);
 
-      $num = (int)$arrayPosts[$index]->likes;
-      if($_POST["like"] == "add"){
-         $arrayPosts[$index]->likes = ++$num;
-      }else{
-         $arrayPosts[$index]->likes = --$num;
-      }
-      updateJson("../../JSON/posts.json", $arrayPosts);
-   }
-
-   //update likes.json
    $arrayLikes = json_decode(file_get_contents("../../JSON/likes.json"));
    $index = findIndex($arrayLikes, "user_id", $_SESSION["loggedUserID"]);
-   $arrayLikes[$index]->posts += ["post_id" => $_POST["post-id"]];
+
+   if($arrayPosts[$indexPosts]){
+
+      $num = (int)$arrayPosts[$indexPosts]->likes;
+      if($_POST["like"] == "add"){
+         $arrayPosts[$indexPosts]->likes = ++$num;
+
+         if($arrayLikes[$index]){
+            $arrayLikes[$index]->posts[] = $_POST["post-id"];
+         }
+      }else{
+         $arrayPosts[$indexPosts]->likes = --$num;
+         if($arrayLikes[$index]){
+            array_splice($arrayLikes[$index]->posts, array_search($_POST["post-id"],$arrayLikes[$index]->posts), 1);
+         }
+      }
+   }
+   updateJson("../../JSON/posts.json", $arrayPosts);
    updateJson("../../JSON/likes.json", $arrayLikes);
 }
