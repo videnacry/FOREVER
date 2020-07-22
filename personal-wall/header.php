4e5ec85572bd5b295118f0b0f3f402a3bfac9f3a
beron <bleink@outlook.es>
2020-07-22 09:37:39 +0200

    added dropdown in header, needed to fix its position

--- personal-wall/header.php
+++ personal-wall/header.php
@@ -1,6 +1,6 @@
 
         <div class="nabvar navbar-dark bg-primary text-light px-4 pt-2 d-flex justify-content-between sticky-top">
-            <h2 class="nabvar-brand">Goldtato</h2>
+            <h2 class="navbar-brand">Goldtato</h2>
             <div>
                 <button class="btn" type="button">
                     <h3><i class="fas fa-bell text-light"></i></h3>
@@ -13,25 +13,17 @@
                         <button id="log-out" class="btn btn-block btn-light mb-2">Log out</button>
                     </form>
                 </div>
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="user-options" data-toggle="collapse" role="button">
-                        <h3><i class="fas fa-user text-light"></i></h3>
-                    </a>
-                    <div class="dropdown-menu">
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item">Update information</a>
-                    </div>
-                </div>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                <div class="nav-item dropdown d-inline">
+                  <a class="nav-link dropdown-toggle width-fit mx-auto" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <h3><i class="fas fa-user text-light"></i></h3>
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
+                    <button id="log-out" class="btn btn-block btn-light mb-2"><span class="dropdown-item">Update information</span></button>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <form action="../login/forms/logout.php" method="get">
+                        <button id="log-out" class="btn btn-block btn-light mb-2">Log out</button>
+                    </form>
                   </div>
-                </li>
+                </div>
             </div>
         </div> 
