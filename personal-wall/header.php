ffc3bdb5596021a38b807358c540d84c6232bdc6
beron <bleink@outlook.es>
2020-07-22 09:49:46 +0200

    fixed position of user icon in header

--- personal-wall/header.php
+++ personal-wall/header.php
@@ -1,18 +1,10 @@
 
         <div class="nabvar navbar-dark bg-primary text-light px-4 pt-2 d-flex justify-content-between sticky-top">
             <h2 class="navbar-brand">Goldtato</h2>
-            <div>
+            <div class="d-flex">
                 <button class="btn" type="button">
                     <h3><i class="fas fa-bell text-light"></i></h3>
                 </button>
-                <button class="btn" type="button" data-toggle="collapse" data-target="#user-options">
-                    <h3><i class="fas fa-user text-light"></i></h3>
-                </button>
-                <div class="collapse navbar-collapse" id="user-options">
-                    <form action="../login/forms/logout.php" method="get">
-                        <button id="log-out" class="btn btn-block btn-light mb-2">Log out</button>
-                    </form>
-                </div>
                 <div class="nav-item dropdown d-inline">
                   <a class="nav-link dropdown-toggle width-fit mx-auto" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <h3><i class="fas fa-user text-light"></i></h3>
